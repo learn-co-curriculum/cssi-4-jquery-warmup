66b5db4311a7c2b4f5f88eabf9b1428ebf8bb8b7
Victoria Kirst <vrk@chromium.org>
2015-07-23 00:04:25 -0400

    sigh

---
+++ cookies/cookies-script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  // 1. Log a message "Baking a cookie" to the console when the "BAKE a cookie"
+  // button is clicked.
+  // Hint: You may need to modify cookies.html as well!
+
+
+  // 2. Implement "BAKE a cookie": Insert an image with source cookie.png into
+  // the div with id="plate" when the "BAKE a cookie" button is clicked.
+  // You should be able to add multiple cookies into the div.
+
+
+  // 3. Implement "EAT ALL the cookies": Remove all the cookies from the pink
+  // plate when "EAT ALL the cookies" button is clicked.
+  // Hint: You may need to modify cookies.html as well!
+
+
+  // 4. Implement "EAT a cookie": Remove a single cookie from the pink plate
+  // when the  "EAT a cookie" button is clicked.
+  // Hint: You may need to modify cookies.html as well!
+
+
+
+});
